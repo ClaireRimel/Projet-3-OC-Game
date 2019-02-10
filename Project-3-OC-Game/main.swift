ed348805b7bdfabd6c57fbce30ddf97604cc7f69
ClaireRimel <sivadiercl@gmail.com>
2019-02-10 18:30:17 +0100

    Initial Commit

---
+++ Project-3-OC-Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project-3-OC-Game
+//
+//  Created by Claire Sivadier on 10/02/2019.
+//  Copyright © 2019 Claire Sivadier. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
